5be8d8ef72cb2aff30dab74a52c8a2bd56a7b2df
Huang Jin <hjinustc@gmail.com>
2019-11-28 03:23:48 +0800

    modify base url

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+  BASE_API: '"http://47.96.119.251:8080"'
 })
